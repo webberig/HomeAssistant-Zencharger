a4ffedd762b3fd4d8fe8d71731bcc562b9f3a73b
Mathieu Maes <mathieu@webberig.be>
2024-11-11 20:23:04 +0000

    Add/fix private methods to perform authenticated GET/POST/PATCH calls to API

--- custom_components/zencharger/zencharger/api.py
+++ custom_components/zencharger/zencharger/api.py
@@ -24,10 +24,11 @@ class ZenchargerApi:
 
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry):
         self._sessionId = None
+        self.cookies = None
         if isinstance(entry, dict):
             self._host = entry[CONF_DATA][CONF_CREDENTIALS][CONF_HOST]
             self._password = entry[CONF_DATA][CONF_CREDENTIALS][CONF_PASSWORD]
-        else:            
+        else:
             self._host = entry.data[CONF_CREDENTIALS][CONF_HOST]
             self._password = entry.data[CONF_CREDENTIALS][CONF_PASSWORD]
 
@@ -55,6 +56,7 @@ class ZenchargerApi:
 
             if "Set-Cookie" in response.headers:
                 self._sessionId = response.headers["Set-Cookie"]
+                self.cookies = response.cookies
                 return response.headers.get("Set-Cookie")
 
             raise ZenchargerApiError("Could not login with given credentials")
@@ -81,18 +83,23 @@ class ZenchargerApi:
         except Exception as error:
             raise ZenchargerApiError("Could not get status")
 
-    def _do_call(self, url: str, body: dict):
+    def __request(self, method: str, path: str, body: dict):
+        """Perform POST or PATCH call to API"""
         if self._sessionId is None:
             self.login()
 
+        url = self._host + "/api/v1/" + path
+
         headers = {
-            "accept": "application/json",
-            "xsrf-token": self._sessionId,
+            "accept": "application/json"
         }
 
         try:
-            response = httpx.post(url, headers=headers, json=body, timeout=5)
+            response = httpx.request(method, url, headers=headers, cookies=self.cookies, json=body, timeout=5)
             response.raise_for_status()
+            if not 'application/json' in response.headers.get('Content-Type', ''):
+                return None
+
             json_data = response.json()
 
             # Session Expired code?
@@ -106,9 +113,37 @@ class ZenchargerApi:
                     f"Retrieving the data for {url} failed with failCode: {json_data[ATTR_FAIL_CODE]}, message: {json_data[ATTR_DATA]}"
                 )
 
-            if ATTR_DATA not in json_data:
+            return json_data
+
+        except KeyError as error:
+            _LOGGER.error(error)
+            _LOGGER.error(response.text)
+
+    def __get(self, path: str):
+        """Perform GET call to API"""
+        if self._sessionId is None:
+            self.login()
+
+        url = self._host + "/api/v1/" + path
+
+        headers = {
+            "accept": "application/json"
+        }
+
+        try:
+            response = get(url, headers=headers, cookies=self.cookies, timeout=1.5)
+            response.raise_for_status()
+            json_data = response.json()
+
+            # Session Expired code?
+            if ATTR_FAIL_CODE in json_data and json_data[ATTR_FAIL_CODE] == 305:
+                # token expired
+                self._sessionId = None
+                return self._do_call(url, body)
+
+            if ATTR_FAIL_CODE in json_data and json_data[ATTR_FAIL_CODE] != 0:
                 raise ZenchargerApiError(
-                    f"Retrieving the data failed. Raw response: {response.text}"
+                    f"Retrieving the data for {url} failed with failCode: {json_data[ATTR_FAIL_CODE]}, message: {json_data[ATTR_DATA]}"
                 )
 
             return json_data
